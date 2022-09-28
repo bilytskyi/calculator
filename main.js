d0c7d753c67ab7f0aaa2e901ea20d624255c5989
Andrii Bilytskyi <abilytskyi@gmail.com>
2022-09-28 12:22:38 +0200

    version work with = sign

--- main.js
+++ main.js
@@ -14,61 +14,50 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(allParts, allResults) {
-    if (allParts.length == 2) {
-
-        let result = parseInt(allParts.slice(-1)) + parseInt(allParts.slice(-2, -1));
-        allResults.push(result);
-
-        display.textContent = result;
-        flag = "on";
-    }
-    if (allParts.length > 2) {
-        let result = parseInt(allParts.slice(-1)) + allResults.slice(-1);
-        allResults.push(result);
-
-        display.textContent = result;
-        flag = "on";
-    }
-    else {
-        display.textContent = "0";
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
     }
 }
 
 function populate() {
-
+    let arr = [];
+    let operator = "";
+    const display = document.querySelector('.display');
+    const buttons = document.querySelectorAll('.button');
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            if (button.getAttribute('type') == "number") {
+                if (display.textContent == "0") {
+                    display.textContent = button.getAttribute('value');
+                }
+                else {
+                    display.textContent += button.getAttribute('value');
+                }
+            }
+            if (button.getAttribute('type') == "clear") {
+                display.textContent = "0";
+            }
+            if (button.getAttribute('type') == "operator") {
+                arr.push(display.textContent);
+                operator = button.getAttribute('value');
+                display.textContent = "0";
+            }
+            if (button.getAttribute('value') == "=") {
+                arr.push(display.textContent);
+                let b = parseInt(arr.slice(-1));
+                let a = parseInt(arr.slice(-2, -1));
+                display.textContent = operate(operator, a, b);
+            }
+        });
+    });
 }
 
-let allParts = [];
-let allResults = [];
-let curr = "0";
-let flag = "on";
-
-
-const display = document.querySelector('.display');
-const buttons = document.querySelectorAll('.button');
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        if (button.getAttribute('type') == "number") {
-            if (display.textContent == "0" || flag == "on") {
-                display.textContent = button.getAttribute('value');
-                curr = button.getAttribute('value');
-                flag = "off";
-            }
-            else {
-                display.textContent += button.getAttribute('value');
-                curr += button.getAttribute('value');
-            }
-        }
-        if (button.getAttribute('type') == "clear") {
-            display.textContent = "0";
-            curr = "0";
-        }
-        if (button.getAttribute('type') == "operator") {
-            allParts.push(curr);
-            display.textContent = "0";
-            operate(allParts, allResults);
-            console.log(allParts);
-        }
-    });
-});
+populate();
